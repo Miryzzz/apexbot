d4bb3087511dbeb4cfe46d2bde98ed5bee5e982f
Matvey <matacrwt2007@gmail.com>
2026-02-06 16:54:17 +0500

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -281,6 +281,6 @@ class handler(BaseHTTPRequestHandler):
 
 def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write(b'Apex Bot is Running!')
+        self.wfile.write("Бот работает! Отправь сообщение в Telegram.".encode('utf-8'))
